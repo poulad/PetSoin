da7d3cc49ce072dd95d66b5b7b8154bdc18e8adc
Poulad <poulad.pld@gmail.com>
2016-06-23 19:06:28 -0400

    Added querying Pet table

--- index.php
+++ index.php
@@ -1,9 +1,49 @@
-<?php
-<<<<<<< HEAD
-phpinfo();
-=======
+<!DOCTYPE html>
 
-phpinfo();
+<html>
+   <head>
+   <title>PetSoin</title>
+   <link rel="stylesheet" href="resources/styles.css">
+   <style>
 
->>>>>>> bdd8f694317ee594b144221b92e4745afb9f622f
+   </style>
+   </head>
+   <body>
+      <h3>Pet</h3>
+      <table>
+         <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Breed</th>
+            <th>Birthdate</th>
+            <th>Weight</th>
+         </tr>
+<?
+try {
+      $oConn = new PDO(
+      "mysql:host=us-cdbr-azure-west-c.cloudapp.net;dbname=petsoin", 
+      "b19257a1ad0368", 
+      "08ac5ec15b6233e");
+      $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $oStmt = $oConn->prepare("select * from pet");
+      $oStmt->execute();
+
+      $oResult = $oStmt->fetchAll();
+      foreach ($oResult as $aRow) {
+         echo "<tr>";
+            echo "<td>".$aRow['pet_id']."</td>";
+            echo "<td>".$aRow['name']."</td>";
+            echo "<td>".$aRow['sex']."</td>";
+            echo "<td>".$aRow['breed']."</td>";
+            echo "<td>".$aRow['birthdate']."</td>";
+            echo "<td>".$aRow['weight']."</td>";
+         echo "</tr>";
+      }
+   } catch(PDOException $e) {
+      echo 'ERROR: ' . $e->getMessage();
+   }
 ?>
+      </table>
+   </body>
+</html>
