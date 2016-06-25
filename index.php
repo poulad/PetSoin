2966db7877b0342f3ac5d170fd296ac5ce413469
Poulad <poulad.pld@gmail.com>
2016-06-25 12:35:53 -0400

    just some css

--- index.php
+++ index.php
@@ -8,14 +8,15 @@
    <link href="public/css/styles.css" media="all" rel="stylesheet" type="text/css" />
    <link href="public/css/public.css" media="all" rel="stylesheet" type="text/css" />
 
-   <div id="page">
-      <div class="index">
-         <img src="public/image/logo.png">
-         <h2>PetSoin: A Pet Health Care System</h2>
-         <p>
-            A growing number of people tend to own a pet and therefore there is a need of a better health care system for our beloved pets. The number of veterinary clinics is increasing that means new clinics need to be managed well to serve the pet owners and pets well. The access to pet’s medical conditions and treatment records is essential to the veterinarians. 
-         </p>
-      </div>
+   <div class='index-page' id="page">
+      <img src="public/image/logo.png">
+      <h2>A Pet Health Care System</h2>
+      <p>
+         A growing number of people tend to own a pet and therefore there is a need of a better health care system for our beloved pets. The number of veterinary clinics is increasing that means new clinics need to be managed well to serve the pet owners and pets well. The access to pet’s medical conditions and treatment records is essential to the veterinarians. 
+      </p>
+      <br>
+      <h3></h3>
+      <p></p>
    </div>
 </div>
 
